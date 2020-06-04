07c9c5cc24f6f0a09f548c0685c2c6c29c2aa2c5
xupeng <xupeng@sensetime.com>
2020-06-04 14:23:25 +0800

    up

---
+++ src/test/java/com/example/demo/MathTest.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import org.apache.commons.collections4.MapUtils;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author: Cirmons
+ * @Date: 2020-05-20
+ */
+public class MathTest {
+    
+    @Test
+    public void math(){
+        
+//        int a = 1;
+//        int b = 21;
+//        System.out.println((float)a/(float)b);
+        
+        
+        String a = "";
+        JSONArray objects = JSON.parseArray(a);
+    
+        System.out.println(1);
+    
+    }
+    
+    
+    @Test
+    public void testMap(){
+        List<String> strings = Arrays.asList("a1", "a2", "a3", "a4");
+        List<String> sub = strings.subList(0, 2);
+        strings.removeAll(sub);
+    
+        System.out.println(1);
+    
+    }
+    
+}
