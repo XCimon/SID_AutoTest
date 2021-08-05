206e757790e8bef0c89e6c011831014d0b4182f2
xupeng <xupeng@sensetime.com>
2021-08-05 15:28:56 +0800

    hash crash

---
+++ src/test/java/com/example/demo/HashTest.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.junit.Test;
+
+/**
+ * @Author: Cirmons
+ * @Date: 2021-08-05
+ */
+public class HashTest {
+    
+    
+    @Test
+    public void hashString(){
+    
+        System.out.println("Aa".hashCode());
+        System.out.println("BB".hashCode());
+        
+    }
+    
+    
+}
